61f0e27c5811d22fa4e64b03ec8538337d60a9ee
Patrick-Mondala <patrickmondala@gmail.com>
2019-08-05 16:20:36 -0700

    w6d1 hw completed

--- W6D1/Map.rb
+++ W6D1/Map.rb
@@ -1,2 +1,25 @@
 class Map
+    def initialize
+      @map = []
+    end
+
+    def set(key, value)
+      map.push([key, value])
+    end
+
+    def get(key)
+      map.each {|pair| return pair.last if pair.first == key}
+      nil
+    end
+
+    def delete(key)
+      map.each {|pair| map.delete(pair) if pair.first == key}
+    end
+
+    def show
+      map.dup
+    end
+
+    :private
+    attr_reader :map
 end
--- W6D1/Queue.rb
+++ W6D1/Queue.rb
@@ -1,2 +1,20 @@
 class Queue
+    def initialize
+      @queue = []
+    end
+
+    def enqueue(el)
+      queue.push(el)
+    end
+
+    def dequeue
+      queue.shift
+    end
+
+    def peek
+      queue.dup
+    end
+
+    :private
+    attr_reader :queue
 end
