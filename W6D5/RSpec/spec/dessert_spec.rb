5aa258289f17be0d48ac1e532afe7f98923bbcd1
Patrick-Mondala <patrickmondala@gmail.com>
2019-08-09 08:02:48 -0700

    used 'before' on mix spec

--- W6D5/RSpec/spec/dessert_spec.rb
+++ W6D5/RSpec/spec/dessert_spec.rb
@@ -32,10 +32,12 @@ describe Dessert do
   end
 
   describe "#mix!" do
+      before(:each) do
+        cake.add_ingredient("flour")
+        cake.add_ingredient("egg")
+        cake.add_ingredient("milk")
+      end
     it "shuffles the ingredient array" do
-      cake.add_ingredient("flour")
-      cake.add_ingredient("egg")
-      cake.add_ingredient("milk")
       original_ingredients = cake.ingredients.dup
       expect(cake.mix!).to_not eq(original_ingredients)
       expect(cake.mix!).to be(cake.ingredients)
