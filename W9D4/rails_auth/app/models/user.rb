e4170c593d48868471abe27aca35eafef5afad95
Patrick-Mondala <patrickmondala@gmail.com>
2019-08-29 01:06:31 -0700

    rails auth user model & table

---
+++ W9D4/rails_auth/app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+    attr_reader :password
+    
+    after_initialize :ensure_session_token
+    validates :username, :session_token, presence: true
+    validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+end
