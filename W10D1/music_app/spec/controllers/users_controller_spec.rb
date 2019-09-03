540976cd2cebae387da15d22774b89be6a505b12
Patrick-Mondala <patrickmondala@gmail.com>
2019-09-02 19:44:16 -0700

    CSS review

--- W10D1/music_app/spec/controllers/users_controller_spec.rb
+++ W10D1/music_app/spec/controllers/users_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UsersController, type: :controller do
   describe "GET #new" do
     it "renders the new template" do
       get :new, {}
-      expect(response).to render_template("new")
+      expect(response).to render_template(:new)
       expect(response).to have_http_status(200)
     end
   end
@@ -13,13 +13,13 @@ RSpec.describe UsersController, type: :controller do
     context "with invalid params" do
       it "validates the presence of the user's email" do
         post :create, params: { user: {email: "", password: "bad_password" } }
-        expect(response).to render_template("new")
+        expect(response).to render_template(:new)
         expect(flash[:errors]).to be_present
       end
 
       it "validates that the password is at least 6 characters long" do
         post :create, params: { user: {email: "fake@fake.com", password: "" } }
-        expect(response).to render_template("new")
+        expect(response).to render_template(:new)
         expect(flash[:errors]).to be_present      
       end
     end
